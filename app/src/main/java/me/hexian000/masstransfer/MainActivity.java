aad36a41225fc3a3d9207f8f0278993217fd32fd
hexian000 <he.xian@163.com>
2018-03-29 11:55:14 +0800

    folder picker

--- app/src/main/java/me/hexian000/masstransfer/MainActivity.java
+++ app/src/main/java/me/hexian000/masstransfer/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,14 +19,15 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends Activity {
-	TransferService mService;
+	private static final int READ_REQUEST_CODE = 42;
+	DiscoverService mService;
 	Timer timer;
 	Handler refresh = new Handler();
 	private ServiceConnection mConnection = new ServiceConnection() {
 		@Override
 		public void onServiceConnected(ComponentName className, IBinder service) {
-			TransferService.TransferServiceBinder binder =
-					(TransferService.TransferServiceBinder) service;
+			DiscoverService.Binder binder =
+					(DiscoverService.Binder) service;
 			mService = binder.getService();
 		}
 
@@ -37,10 +39,10 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onPause() {
-		Intent intent1 = new Intent(MainActivity.this, TransferService.class);
+		Intent intent1 = new Intent(MainActivity.this, DiscoverService.class);
 		unbindService(mConnection);
 		stopService(intent1);
-		Log.d(TransferApp.LOG_TAG, "stop TransferService");
+		Log.d(TransferApp.LOG_TAG, "stop DiscoverService");
 		if (timer != null) {
 			timer.cancel();
 			timer = null;
@@ -50,10 +52,10 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onResume() {
-		Intent intent1 = new Intent(MainActivity.this, TransferService.class);
+		Intent intent1 = new Intent(MainActivity.this, DiscoverService.class);
 		bindService(intent1, mConnection, Context.BIND_AUTO_CREATE);
 		startService(intent1);
-		Log.d(TransferApp.LOG_TAG, "start TransferService");
+		Log.d(TransferApp.LOG_TAG, "start DiscoverService");
 
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
@@ -70,6 +72,11 @@ public class MainActivity extends Activity {
 								android.R.layout.simple_list_item_1,
 								items);
 						peersList.setAdapter(adapter);
+						peersList.setOnItemClickListener((adapterView, view, i, l) -> {
+							String s = (String) adapter.getItem(i);
+							Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+							pickFolder();
+						});
 					});
 				} else {
 					refresh.post(() -> {
@@ -83,6 +90,13 @@ public class MainActivity extends Activity {
 		}, 0, 500);
 
 		super.onResume();
+	}
+
+	private void pickFolder() {
+		Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		intent.setType("*/*");
+		startActivityForResult(intent, READ_REQUEST_CODE);
 	}
 
 	@Override
