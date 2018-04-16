ef670cedcfe3278d312edb9745385702483305f9
hexian000 <he.xian@163.com>
2018-04-16 10:16:06 +0800

    format

--- app/src/main/java/me/hexian000/masstransfer/io/DirectoryReader.java
+++ app/src/main/java/me/hexian000/masstransfer/io/DirectoryReader.java
@@ -51,8 +51,7 @@ public class DirectoryReader implements Runnable {
 		}
 		byte[] path = pathStr.getBytes("UTF-8");
 		ByteArrayOutputStream header = new ByteArrayOutputStream();
-		ByteBuffer lengths = ByteBuffer.allocate(Integer.BYTES + Long.BYTES).
-				order(ByteOrder.BIG_ENDIAN);
+		ByteBuffer lengths = ByteBuffer.allocate(Integer.BYTES + Long.BYTES).order(ByteOrder.BIG_ENDIAN);
 		lengths.putInt(path.length);
 		Log.d(LOG_TAG, "Now at: " + pathStr);
 		lengths.putLong(-1); // directory
@@ -81,8 +80,7 @@ public class DirectoryReader implements Runnable {
 		}
 		byte[] path = pathStr.getBytes("UTF-8");
 		ByteArrayOutputStream header = new ByteArrayOutputStream();
-		ByteBuffer lengths = ByteBuffer.allocate(Integer.BYTES + Long.BYTES).
-				order(ByteOrder.BIG_ENDIAN);
+		ByteBuffer lengths = ByteBuffer.allocate(Integer.BYTES + Long.BYTES).order(ByteOrder.BIG_ENDIAN);
 		lengths.putInt(path.length);
 		final String name = file.getName();
 		Log.d(LOG_TAG, "sendFile: " + name + " length=" + file.length());
@@ -128,8 +126,7 @@ public class DirectoryReader implements Runnable {
 				}
 			}
 			reporter.report(null, 0, 0);
-			ByteBuffer lengths = ByteBuffer.allocate(Integer.BYTES + Long.BYTES).
-					order(ByteOrder.BIG_ENDIAN);
+			ByteBuffer lengths = ByteBuffer.allocate(Integer.BYTES + Long.BYTES).order(ByteOrder.BIG_ENDIAN);
 			lengths.putInt(0);
 			lengths.putLong(0);
 			out.write(lengths.array()); // bye
