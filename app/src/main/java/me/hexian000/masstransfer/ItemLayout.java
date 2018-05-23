7da96ea20793b9bbcb2a2bd57f6cbed4cb8fdecd
hexian000 <he.xian@163.com>
2018-05-23 17:57:24 +0800

    Revert: remove unused constructors causing app FC

--- app/src/main/java/me/hexian000/masstransfer/ItemLayout.java
+++ app/src/main/java/me/hexian000/masstransfer/ItemLayout.java
@@ -1,6 +1,7 @@
 package me.hexian000.masstransfer;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.CheckBox;
 import android.widget.Checkable;
 import android.widget.RelativeLayout;
@@ -10,6 +11,14 @@ public class ItemLayout extends RelativeLayout implements Checkable {
 
 	public ItemLayout(Context context) {
 		super(context);
+	}
+
+	public ItemLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public ItemLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
 	}
 
 	@Override
