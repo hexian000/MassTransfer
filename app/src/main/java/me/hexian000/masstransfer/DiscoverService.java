064caa88e621d8febbf3d40b805031f72edb2b40
hexian000 <he.xian@163.com>
2018-06-06 14:46:43 +0800

    acquire multicast lock for peer discover

--- app/src/main/java/me/hexian000/masstransfer/DiscoverService.java
+++ app/src/main/java/me/hexian000/masstransfer/DiscoverService.java
@@ -2,6 +2,7 @@ package me.hexian000.masstransfer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -9,8 +10,29 @@ import android.util.Log;
 import static me.hexian000.masstransfer.MassTransfer.LOG_TAG;
 
 public class DiscoverService extends Service {
+	private static WifiManager.MulticastLock multicastLock = null;
 	private final IBinder mBinder = new Binder();
 	Discoverer discoverer = null;
+
+	void acquireLock() {
+		releaseLock();
+		WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+		if (wifiManager != null) {
+			multicastLock = wifiManager.createMulticastLock(LOG_TAG);
+			multicastLock.acquire();
+			Log.d(LOG_TAG, "MulticastLock acquired");
+		}
+	}
+
+	void releaseLock() {
+		if (multicastLock != null) {
+			if (multicastLock.isHeld()) {
+				multicastLock.release();
+				Log.d(LOG_TAG, "MulticastLock released");
+			}
+			multicastLock = null;
+		}
+	}
 
 	@Override
 	public void onDestroy() {
@@ -19,20 +41,20 @@ public class DiscoverService extends Service {
 			discoverer.close();
 			discoverer = null;
 		}
+		releaseLock();
 	}
 
 	@Override
 	public void onCreate() {
+		acquireLock();
 		discoverer = new Discoverer(MassTransfer.UDP_PORT);
+		Log.d(LOG_TAG, "discover start");
+		discoverer.start();
 	}
 
 	@Nullable
 	@Override
 	public IBinder onBind(Intent intent) {
-		if (discoverer != null) {
-			Log.d(LOG_TAG, "discover start");
-			discoverer.start();
-		}
 		return mBinder;
 	}
 
